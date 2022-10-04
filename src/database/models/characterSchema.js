3179ea0322dd595e4c33c0af6ba93c6fbd2ad83f
Your NameDevbruno08 <bruno_emonteiro@hotmail.com>
2022-10-03 21:47:36 -0300

    feat: adjustment schema

--- src/database/models/characterSchema.js
+++ src/database/models/characterSchema.js
@@ -5,7 +5,7 @@ const characterSchema = new Schema({
   name: { type: String, required: true },
   title: { type: String, required: true },
   gender: { type: String, required: true },
-  imageURL:{ type: String, required: true}
+  imageURL:{ type: String, required: true},
 });
 
 const character = model('characters', characterSchema);
